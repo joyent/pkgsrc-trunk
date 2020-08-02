42bf2e7f2eae61fcfbeda3783366809a59758e31
Adeel <adeelbm@outlook.com>
2020-08-02 14:10:13 +0300

    Update userspace-rcu from v0.7.3 to v0.12.1

--- devel/userspace-rcu/buildlink3.mk
+++ devel/userspace-rcu/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	userspace-rcu
 .if !defined(USERSPACE_RCU_BUILDLINK3_MK)
 USERSPACE_RCU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.userspace-rcu+=	userspace-rcu>=0.7.3
+BUILDLINK_API_DEPENDS.userspace-rcu+=	userspace-rcu>=0.12.1
 BUILDLINK_PKGSRCDIR.userspace-rcu?=	../../devel/userspace-rcu
 .endif	# USERSPACE_RCU_BUILDLINK3_MK
 
